457e74a22355e6382dcc1d62ac2d1171f0bbf41a
ionutcutean <104094424+ionutcutean@users.noreply.github.com>
2022-10-19 13:21:47 +0300

    Add files via upload

--- locatie.js
+++ locatie.js
@@ -6,7 +6,7 @@ function on_success(position) {
     document.getElementById("acc").innerHTML = "Accuracy   = " + position.coords.accuracy;
     document.getElementById("altitude").innerHTML = "Altitude  = " + position.coords.altitude;
     var latlon = position.coords.latitude + "," + position.coords.longitude;
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&key=AIzaSyAm1NI-K8xEkLLIvaysMoZ9xRNEwooj_tU";
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&key=AIzaSyBWCPFp7jY6LWG9rujeaZujHgQzExmDOl4";
     document.getElementById("map").innerHTML = "<img src='" + img_url + "'>";
 }
 
